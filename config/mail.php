38254b9a8a281b3cebb5581eab8ac0f821db3ff4
“seulgikun” <junriclimpangog25@gmail.com>
2026-09-13 07:02:42 +0800

    Set SMTP timeout to 5s to avoid 30s PHP fatal error when SMTP port is blocked

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 5,
             'auth_mode' => null,
         ],
 
